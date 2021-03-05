2056e1d65cb791a12c91dcf687e2d2ad95ccf143
andriy.nikiforov <andriy@rexsoftinc.com>
2021-03-05 11:16:21 +0200

    Template finish.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import history from './utils/history';
+
+import Routes from './routes';
 
 ReactDOM.render(
-  <React.StrictMode />,
+  <Provider store={}>
+    <Router history={history}>
+      <Routes />
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <title>Title</title>
+      </Helmet>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
 
